910814336e5b26a14687a3907d2054699ca9f726
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-03 10:43:27 +0100

    Add basic struct of project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+import { notFound, catchErrors } from "./middlewares/errors.js";
+import config from "./config/config.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Views
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Errors handling
+app.use(notFound);
+app.use(catchErrors);
+
+// Server state
+app.listen(config.server.port, () => {
+    console.log(`Server is listen on: http://localhost:${config.server.port}`);
+})
