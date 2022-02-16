c1fd9d32043aed1736aa43403e9fcba07284d96e
KamilFilar <49237105+KamilFilar@users.noreply.github.com>
2022-02-16 13:37:28 +0100

    Update newsletterController

--- src/controllers/newsletterController.js
+++ src/controllers/newsletterController.js
@@ -3,7 +3,8 @@ import pool from "./../config/dbConfig.js"
 export default {
     
     async addEmail(req, res, next) {
-        const query = "INSERT INTO `emails` (`name`, `created_at`) VALUES (?, ?);";
+        const queryEXIST = "SELECT * FROM `emails` WHERE `name` = ?";
+        const queryINSERT = "INSERT INTO `emails` (`name`, `created_at`) VALUES (?, ?);";
         pool.getConnection((err, connection) => {
             if (err) 
                 throw err;
@@ -11,11 +12,26 @@ export default {
             const params = req.body;
             const date = new Date();
 
-            connection.query(query, [params.email, date], (err) => {
+            connection.query(queryEXIST, [params.email, date], (err, row) => {
                 if (!err) {
-                    return res.status(200).send(
-                        `Email: ${params.email} has been added succesfull!`
-                    );
+                   if(row.length == 1){
+                        return res.status(400).send({
+                            err: "This email already exist!"
+                        });
+                   }
+                   else {
+                    connection.query(queryINSERT, [params.email, date], (err) => {
+                        if (!err) {
+                            return res.status(200).send({
+                                msg: `Email: ${params.email} has been added succesfull!`
+                            });
+                        }
+                        else {
+                            console.log(err);
+                            return res.status(400).send(err);
+                        }
+                    })
+                   }
                 }
                 else {
                     console.log(err);
